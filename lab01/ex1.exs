0a8ffc894dc1b3ebc88260e08da0f56fe1c118ee
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-03-25 14:15:28 +0100

    save materials

--- lab01/ex1.exs
+++ lab01/ex1.exs
@@ -12,27 +12,4 @@ defmodule Ex1 do
 
     Enum.find(arr, &(&1 in most_comon))
   end
-
-  # def tree(arr) do
-  #   [{_, max} | _] =
-  #     common =
-  #     Enum.reduce(arr, :gb_trees.empty(), fn key, tree ->
-  #       :gb_trees.enter(key, get(tree, key, 0) + 1, tree)
-  #     end)
-  #     |> :gb_trees.to_list()
-  #     |> Enum.reverse()
-
-  #   most_comon =
-  #     Enum.filter(common, fn {_, x} -> x == max end)
-  #     |> Enum.map(fn {v, _} -> v end)
-
-  #   Enum.find(arr, &(&1 in most_comon))
-  # end
-
-  # defp get(tree, key, default) do
-  #   case :gb_trees.lookup(key, tree) do
-  #     {:value, value} -> value
-  #     :none -> default
-  #   end
-  # end
 end
